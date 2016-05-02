543d01741e174bc689424e4ae36f5639d4ce9758
Charles Kaminer <ckamine@g.clemson.edu>
2016-05-02 13:19:55 -0700

    added background color feature

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x = rand(101)
+number = rand(101)
 
 get '/' do
-  "The SECRET NUMBER is #{x}"
+  result = check_guess(number)
+  final = final(number)
+  color = change_color(number)
+  erb :index, :locals => {:number => number, :result => result, :final => final,
+                          :color => color}
+end
+
+def check_guess(number)
+  if params['guess'].to_i > number + 5
+    "Way too high!"
+  elsif
+    params['guess'].to_i < number - 5
+    "Way too low!"
+  elsif params['guess'].to_i > number
+    "Too high!"
+  elsif params['guess'].to_i < number
+    "Too low!"
+  else
+    return "You got it right!"
+  end
+end
+
+def final(number)
+  if check_guess(number) == "You got it right!"
+    "The SECRET NUMBER is #{number}"
+  end
+end
+
+def change_color(number)
+  if params['guess'].to_i > number + 5
+    'red'
+  elsif params['guess'].to_i < number - 5
+    'red'
+  elsif params['guess'].to_i > number
+    'IndianRed'
+  elsif params['guess'].to_i < number
+    'IndianRed'
+  else
+    'green'
+  end
 end
